d2f0bd8da4429a04d53546fded33067a380b6418
Sergey Semenyuk <sergey.semenyuk@gentechpartners.com>
2018-05-25 11:30:28 +0300

    add templates and starts pages

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView
+from .models import Todo
+
+
+class HomePageView(ListView):
+    model = Todo
+    template_name = 'home.html'
